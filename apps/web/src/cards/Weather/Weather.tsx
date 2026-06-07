a5d34331dd1a4c4d8648758b3d88739a59e9adce
marcinm <mm.kodmax@gmail.com>
2026-06-07 17:21:47 +0200

    changed wind speed to m/s

--- apps/web/src/cards/Weather/Weather.tsx
+++ apps/web/src/cards/Weather/Weather.tsx
@@ -15,98 +15,97 @@ import { sunTimes } from './sunTimes'
 export const Weather: FC<Record<string, never>> = () => {
   const [weather, updatedAt] = useUpdate<WeatherData>('weather')
 
-  if (weather) {
-    const moonAlt = (getMoonPosition(new Date(), 52.2287755, 20.9756375).altitude / Math.PI) * 180
-    const sunAlt = (getPosition(new Date(), 52.2287755, 20.9756375).altitude / Math.PI) * 180
-    const bs = beaufortScale(weather.instant.wind.speed)
-    const hum = weather.instant.humidity
-    const sun = sunTimes(weather)
-
-    return (
-      <ApolloCard cardId='current-weather' banner={banner} zoomBanner={zoomBanner} updatedAt={updatedAt}>
-        <ZoomContext.Consumer>
-          {zoom => (
-            <table className='apollo-data-table'>
-              <tbody>
-                <tr>
-                  <td>Temperatura</td>
-                  <td>
-                    <HoursBars data={weather.outdoorTemp} positiveMax={30} />
-                  </td>
-                  <td>
-                    <ColorIndicator instant={weather.instant.temp} range={{ optimal: 21, higest: 30, lowest: 15 }} />
-                    {Number(weather.instant.temp).toFixed(0)} °C
-                  </td>
-                </tr>
-                <tr>
-                  <td>Indeks UV</td>
-                  <td></td>
-                  <td>
-                    <ColorIndicator instant={weather.instant.uv} range={{ optimal: 2, higest: 7, lowest: 0 }} />
-                    {weather.instant.uv}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Predkość wiatru</td>
-                  <td style={{ fontSize: '0.5em' }}>{zoom.active ? `${bs} - ${beaufortLevelLabel(bs)}` : `${bs} B`}</td>
-                  <td>
-                    {zoom.active ? (
-                      <NorthIcon
-                        sx={{ transform: `rotate(${weather.instant.wind.angle}deg)`, marginRight: '0.25em' }}
-                      />
-                    ) : null}
-                    <ColorIndicator instant={bs} range={{ lowest: 0, higest: 7, optimal: 1 }} />
-                    {weather.instant.wind.speed} km/h
-                  </td>
-                </tr>
-                <tr>
-                  <td>Wilgotność</td>
-                  <td></td>
-                  <td>
-                    <ColorIndicator instant={hum} range={optimalHumidityRange} />
-                    {hum.toFixed(0)}%
-                  </td>
-                </tr>
-                {!zoom.active || weather.instant.wind.speed === weather.instant.wind.maxSpeed ? null : (
-                  <tr>
-                    <td>Porywy wiatru</td>
-                    <td></td>
-                    <td>{weather.instant.wind.maxSpeed} km/h</td>
-                  </tr>
-                )}
-                {!zoom.active ? null : (
-                  <tr>
-                    <td>Wysokość {sun.timeOfDay === 'day' ? 'słońca' : 'księżyca'}</td>
-                    <td></td>
-                    {sun.timeOfDay === 'day' ? (
-                      <td>
-                        <ColorIndicator instant={sunAlt} range={{ lowest: -6, optimal: 30, higest: 50 }} />
-                        {Number(sunAlt).toFixed(0)}°
-                      </td>
-                    ) : (
-                      <td>{Number(moonAlt).toFixed(0)}°</td>
-                    )}
-                  </tr>
-                )}
-                {!zoom.active ? null : (
-                  <tr>
-                    <td>Zachmurzenie</td>
-                    <td></td>
-                    <td>{weather.instant.clouds.coverage}</td>
-                  </tr>
-                )}
-                {/* <Pressure /> */}
-              </tbody>
-            </table>
-          )}
-        </ZoomContext.Consumer>
-      </ApolloCard>
-    )
-  } else {
+  if (weather === undefined) {
     return (
       <ApolloCard cardId='current-weather' banner={banner}>
         <TablePlaceholder rows={4} graph={true} value={true} />
       </ApolloCard>
     )
   }
+  const moonAlt = (getMoonPosition(new Date(), 52.2287755, 20.9756375).altitude / Math.PI) * 180
+  const sunAlt = (getPosition(new Date(), 52.2287755, 20.9756375).altitude / Math.PI) * 180
+  const bs = beaufortScale(weather.instant.wind.speed)
+  const hum = weather.instant.humidity
+  const sun = sunTimes(weather)
+
+  const windMaxSpeed =
+    weather.instant.wind.speedUnit === 'km/h'
+      ? Math.round(weather.instant.wind.maxSpeed / 3.6)
+      : weather.instant.wind.maxSpeed
+
+  const windSpeed =
+    weather.instant.wind.speedUnit === 'km/h'
+      ? Math.round(weather.instant.wind.speed / 3.6)
+      : weather.instant.wind.speed
+
+  return (
+    <ApolloCard cardId='current-weather' banner={banner} zoomBanner={zoomBanner} updatedAt={updatedAt}>
+      <ZoomContext.Consumer>
+        {zoom => (
+          <table className='apollo-data-table'>
+            <tbody>
+              <tr>
+                <td>Temperatura</td>
+                <td>
+                  <HoursBars data={weather.outdoorTemp} positiveMax={30} />
+                </td>
+                <td>
+                  <ColorIndicator instant={weather.instant.temp} range={{ optimal: 21, higest: 30, lowest: 15 }} />
+                  {Number(weather.instant.temp).toFixed(0)} °C
+                </td>
+              </tr>
+              <tr>
+                <td>Indeks UV</td>
+                <td></td>
+                <td>
+                  <ColorIndicator instant={weather.instant.uv} range={{ optimal: 2, higest: 7, lowest: 0 }} />
+                  {weather.instant.uv}
+                </td>
+              </tr>
+              <tr>
+                <td>Wilgotność</td>
+                <td></td>
+                <td>
+                  <ColorIndicator instant={hum} range={optimalHumidityRange} />
+                  {hum.toFixed(0)}%
+                </td>
+              </tr>
+              <tr>
+                <td>Predkość wiatru</td>
+                <td style={{ fontSize: '0.5em' }}>{zoom.active ? `${bs} - ${beaufortLevelLabel(bs)}` : `${bs} B`}</td>
+                <td>
+                  {zoom.active ? (
+                    <NorthIcon sx={{ transform: `rotate(${weather.instant.wind.angle}deg)`, marginRight: '0.25em' }} />
+                  ) : null}
+                  <ColorIndicator instant={bs} range={{ lowest: 0, higest: 7, optimal: 1 }} />
+                  {windSpeed} m/s
+                </td>
+              </tr>
+              {!zoom.active ? null : (
+                <tr>
+                  <td>Porywy wiatru</td>
+                  <td></td>
+                  <td>{windMaxSpeed} m/s</td>
+                </tr>
+              )}
+              {!zoom.active ? null : (
+                <tr>
+                  <td>Wysokość {sun.timeOfDay === 'day' ? 'słońca' : 'księżyca'}</td>
+                  <td></td>
+                  {sun.timeOfDay === 'day' ? (
+                    <td>
+                      <ColorIndicator instant={sunAlt} range={{ lowest: -6, optimal: 30, higest: 50 }} />
+                      {Number(sunAlt).toFixed(0)}°
+                    </td>
+                  ) : (
+                    <td>{Number(moonAlt).toFixed(0)}°</td>
+                  )}
+                </tr>
+              )}
+            </tbody>
+          </table>
+        )}
+      </ZoomContext.Consumer>
+    </ApolloCard>
+  )
 }
