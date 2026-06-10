9217cb8a65b10d56c9309c34ed842aac89ff0972
marcinm <mm.kodmax@gmail.com>
2026-06-10 23:13:58 +0200

    fixed unused import

--- apps/web/src/cards/StockMarket.tsx
+++ apps/web/src/cards/StockMarket.tsx
@@ -5,7 +5,6 @@ import { refreshFeeds, useUpdate } from '@repo/feed-client'
 import ApolloCard, { ZoomContext } from '../apollo-card/ApolloCard'
 import TablePlaceholder from './components/TablePlaceholder'
 import { StockMarketData } from '@repo/types'
-import { Open } from './Jobs/styled'
 import LinkOpen from './components/LinkOpen'
 
 export const StockMarket: FC<Record<string, never>> = () => {
