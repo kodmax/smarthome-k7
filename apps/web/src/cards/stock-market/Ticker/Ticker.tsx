3bfd77037da0e37d728bbf43dc9c8916c9ab93f2
marcinm <mm.kodmax@gmail.com>
2026-06-27 15:05:03 +0200

    replaced tr with TableRow

--- apps/web/src/cards/stock-market/Ticker/Ticker.tsx
+++ apps/web/src/cards/stock-market/Ticker/Ticker.tsx
@@ -1,15 +1,16 @@
 import { FC } from 'react'
 import { TickerData } from '@repo/types'
 import { EarningsDate, EG, PEAtPT, Price, Symbol } from './cells'
+import { TableRow } from '@mui/material'
 
 export const Ticker: FC<{ ticker: TickerData; zoom: boolean }> = ({ ticker, zoom }) => {
   return (
-    <tr>
+    <TableRow>
       <Symbol ticker={ticker} zoom={zoom} />
       <EarningsDate ticker={ticker} zoom={zoom} />
       <EG ticker={ticker} />
       <PEAtPT ticker={ticker} />
       <Price ticker={ticker} />
-    </tr>
+    </TableRow>
   )
 }
