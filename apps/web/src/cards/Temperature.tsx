452512b39578e0542da1b5d797a5141ee1c18907
marcinm <mm.kodmax@gmail.com>
2026-07-05 13:36:28 +0200

    temperature heating status and target visible only in zoom

--- apps/web/src/cards/Temperature.tsx
+++ apps/web/src/cards/Temperature.tsx
@@ -2,7 +2,7 @@ import { TableBody } from '@mui/material'
 import { type FC } from 'react'
 import { TemperatureIcon } from '@repo/assets'
 import { ApolloDataTable, KnxReading, KnxStateIcon } from '@/card-components'
-import { ApolloCard } from '@/apollo-card'
+import { ApolloCard, ZoomContext } from '@/apollo-card'
 import { Bedtime, Fireplace, LightMode, Air, type SvgIconComponent, ThermostatAuto } from '@mui/icons-material'
 import { TemperatureData } from '@repo/types'
 
@@ -16,69 +16,81 @@ const icons: Record<string, SvgIconComponent> = {
 export const Temperature: FC<Record<string, never>> = () => {
   return (
     <ApolloCard cardId='indoor-temp' title='Temperatura' icon={TemperatureIcon}>
-      <ApolloDataTable>
-        <TableBody>
-          <KnxReading
-            bars={{ historyKey: 'today', highest: 30, lowest: 20, optimal: 24, color: true }}
-            precision={2}
-            feed='home.temp.bathroom-floor'
-            label='Podłoga łazienki'
-            range={{ optimal: 25, lowest: 21, highest: 30 }}
-            icon={
-              <KnxStateIcon<TemperatureData>
-                icon={payload => icons[payload.mode.bathroom.text] ?? ThermostatAuto}
-                id='heating'
-                active={payload => payload.status.lazienkaPodloga.value === 1}
+      <ZoomContext.Consumer>
+        {zoom => (
+          <ApolloDataTable>
+            <TableBody>
+              <KnxReading
+                bars={{ historyKey: 'today', highest: 30, lowest: 20, optimal: 24, color: true }}
+                precision={2}
+                feed='home.temp.bathroom-floor'
+                label='Podłoga łazienki'
+                range={{ optimal: 25, lowest: 21, highest: 30 }}
+                icon={
+                  zoom.active ? (
+                    <KnxStateIcon<TemperatureData>
+                      icon={payload => icons[payload.mode.bathroom.text] ?? ThermostatAuto}
+                      id='heating'
+                      active={payload => payload.status.lazienkaPodloga.value === 1}
+                    />
+                  ) : undefined
+                }
               />
-            }
-          />
-          <KnxReading
-            bars={{ historyKey: 'today', highest: 30, lowest: 20, optimal: 24, color: true }}
-            precision={2}
-            feed='home.temp.livingroom'
-            target={payload => Number(payload.setpoint).toFixed(1)}
-            label='Salon'
-            range={{ optimal: 25, lowest: 21, highest: 30 }}
-            icon={
-              <KnxStateIcon<TemperatureData>
-                icon={payload => icons[payload.mode.livingroom.text] ?? ThermostatAuto}
-                id='heating'
-                active={payload => payload.status.salon.value === 1}
+              <KnxReading
+                bars={{ historyKey: 'today', highest: 30, lowest: 20, optimal: 24, color: true }}
+                precision={2}
+                feed='home.temp.livingroom'
+                target={zoom.active ? payload => Number(payload.setpoint).toFixed(1) : undefined}
+                label='Salon'
+                range={{ optimal: 25, lowest: 21, highest: 30 }}
+                icon={
+                  zoom.active ? (
+                    <KnxStateIcon<TemperatureData>
+                      icon={payload => icons[payload.mode.livingroom.text] ?? ThermostatAuto}
+                      id='heating'
+                      active={payload => payload.status.salon.value === 1}
+                    />
+                  ) : undefined
+                }
               />
-            }
-          />
-          <KnxReading
-            bars={{ historyKey: 'today', highest: 30, lowest: 20, optimal: 24, color: true }}
-            precision={2}
-            feed='home.temp.bedroom'
-            target={payload => Number(payload.setpoint).toFixed(1)}
-            label='Sypialnia'
-            range={{ optimal: 25, lowest: 21, highest: 30 }}
-            icon={
-              <KnxStateIcon<TemperatureData>
-                icon={payload => icons[payload.mode.bedroom.text] ?? ThermostatAuto}
-                id='heating'
-                active={payload => payload.status.sypialnia.value === 1}
+              <KnxReading
+                bars={{ historyKey: 'today', highest: 30, lowest: 20, optimal: 24, color: true }}
+                precision={2}
+                feed='home.temp.bedroom'
+                target={zoom.active ? payload => Number(payload.setpoint).toFixed(1) : undefined}
+                label='Sypialnia'
+                range={{ optimal: 25, lowest: 21, highest: 30 }}
+                icon={
+                  zoom.active ? (
+                    <KnxStateIcon<TemperatureData>
+                      icon={payload => icons[payload.mode.bedroom.text] ?? ThermostatAuto}
+                      id='heating'
+                      active={payload => payload.status.sypialnia.value === 1}
+                    />
+                  ) : undefined
+                }
               />
-            }
-          />
-          <KnxReading
-            bars={{ historyKey: 'today', highest: 30, lowest: 20, optimal: 24, color: true }}
-            precision={2}
-            feed='home.temp.bathroom'
-            target={payload => Number(payload.setpoint).toFixed(1)}
-            label='Łazienka'
-            range={{ optimal: 25, lowest: 21, highest: 30 }}
-            icon={
-              <KnxStateIcon<TemperatureData>
-                icon={payload => icons[payload.mode.bathroom.text] ?? ThermostatAuto}
-                id='heating'
-                active={payload => payload.status.lazienka.value === 1}
+              <KnxReading
+                bars={{ historyKey: 'today', highest: 30, lowest: 20, optimal: 24, color: true }}
+                precision={2}
+                feed='home.temp.bathroom'
+                target={zoom.active ? payload => Number(payload.setpoint).toFixed(1) : undefined}
+                label='Łazienka'
+                range={{ optimal: 25, lowest: 21, highest: 30 }}
+                icon={
+                  zoom.active ? (
+                    <KnxStateIcon<TemperatureData>
+                      icon={payload => icons[payload.mode.bathroom.text] ?? ThermostatAuto}
+                      id='heating'
+                      active={payload => payload.status.lazienka.value === 1}
+                    />
+                  ) : undefined
+                }
               />
-            }
-          />
-        </TableBody>
-      </ApolloDataTable>
+            </TableBody>
+          </ApolloDataTable>
+        )}
+      </ZoomContext.Consumer>
     </ApolloCard>
   )
 }
