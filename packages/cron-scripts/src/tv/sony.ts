ffdac2acc188f1de7a8c8c8a4249993c68fa7886
marcinm <mm.kodmax@gmail.com>
2026-07-08 20:44:50 +0200

    tv cron script update

--- packages/cron-scripts/src/tv/sony.ts
+++ packages/cron-scripts/src/tv/sony.ts
@@ -2,20 +2,29 @@
 import { IpControl } from 'sony-bravia-ip-control'
 import { requireEnv } from '#config/env'
 
-const test = async (sony: IpControl) => {
+const isNoSignal = async (sony: IpControl): Promise<boolean> => {
+  const inputs = await sony.getCurrentExternalInputsStatus()
+  const content = await sony.getPlayingContentInfo()
+
+  const input = inputs.find(externalInput => externalInput.uri === content.uri)
+  return input?.status === 'false'
+}
+
+const run = async (sony: IpControl) => {
   const power = await sony.getPowerStatus()
+  if (power.status !== 'active') {
+    return
+  }
 
-  if (power.status === 'active') {
-    const inputs = await sony.getCurrentExternalInputsStatus()
-    const content = await sony.getPlayingContentInfo()
-
-    const input = inputs.find(externalInput => externalInput.uri === content.uri)
-
-    if (input?.status === 'false') {
-      console.log('Turning off Sony TV because no signal detected')
-      await sony.setPowerStatus('standby')
-    }
+  if (await isNoSignal(sony)) {
+    setTimeout(() => {
+      isNoSignal(sony).then(noSignal => {
+        if (noSignal) {
+          return sony.setPowerStatus('standby')
+        }
+      })
+    }, 60_000)
   }
 }
 
-await test(new IpControl(requireEnv('SONY_TV_IP'), requireEnv('SONY_TV_SECRET')))
+await run(new IpControl(requireEnv('SONY_TV_IP'), requireEnv('SONY_TV_SECRET')))
