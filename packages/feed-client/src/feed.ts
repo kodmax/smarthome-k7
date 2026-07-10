a27ffe373d344592b77ea6c8affd7af06ea09254
marcinm <mm.kodmax@gmail.com>
2026-07-10 19:12:52 +0200

    fixed useFeed issue with late consumption

--- packages/feed-client/src/feed.ts
+++ packages/feed-client/src/feed.ts
@@ -3,8 +3,10 @@ import { WSClient } from './WSClient'
 
 const websocketUrl = import.meta.env.VITE_WEBSOCKET_URL ?? `ws://${location.hostname}:3678`
 const subscribers: Map<string, TopicSubscriber<unknown>[]> = new Map()
+const recentPayload: Map<string, unknown> = new Map()
 
 const wsClient = new WSClient(websocketUrl, ({ topic, payload }) => {
+  recentPayload.set(topic, payload)
   for (const subscriber of subscribers.get(topic) ?? []) {
     subscriber({ topic, payload })
   }
@@ -15,7 +17,15 @@ const subscribe: (topic: string, subscriber: TopicSubscriber<unknown>) => () => 
   topicSubscribers.push(subscriber)
   subscribers.set(topic, topicSubscribers)
 
-  wsClient.subscribe(topic)
+  if (topicSubscribers.length === 1) {
+    wsClient.subscribe(topic)
+  }
+
+  const payload = recentPayload.get(topic)
+  if (payload !== undefined) {
+    subscriber({ topic, payload })
+  }
+
   return () => {
     const topicSubscribers = subscribers.get(topic) ?? []
     topicSubscribers.splice(topicSubscribers.indexOf(subscriber), 1)
