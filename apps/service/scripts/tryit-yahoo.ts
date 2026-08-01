97122191328c5f231abebfb19123c12543e3ca0a
marcinm <mm.kodmax@gmail.com>
2026-08-01 19:04:30 +0200

    remove playground scripts

--- apps/service/scripts/tryit-yahoo.ts
+++
@@ -1,9 +0,0 @@
-import { createLogger } from '@repo/logger'
-
-const logger = createLogger({ name: 'tryit' })
-const ch = logger.child({ component: 'yahoo-fetch' }, { level: 'debug' })
-
-logger.info({ foo: 'bar' }, 'Logger dziala')
-
-ch.debug({ prop: 'child' }, 'debug message')
-ch.info({ something: true }, 'something else')
