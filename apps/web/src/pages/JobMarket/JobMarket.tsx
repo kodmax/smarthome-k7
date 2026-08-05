3a9ccf674c1402ad198dc3d17ccc3ebb0509fed9
marcinm <mm.kodmax@gmail.com>
2026-08-05 12:03:11 +0200

    jobmarket screen layout change

--- apps/web/src/pages/JobMarket/JobMarket.tsx
+++ apps/web/src/pages/JobMarket/JobMarket.tsx
@@ -7,9 +7,7 @@ import { PageHeader } from '@/app/components/PageHeader'
 import { PageWrapper } from '@/app/components/PageWrapper'
 import { useTranslations } from '@/i18n'
 import { ActiveOffers } from './cards/ActiveOffers/ActiveOffers'
-import { MedianSalary } from './cards/MedianSalary/MedianSalary'
 import { P90Salary } from './cards/P90Salary/P90Salary'
-import { NewOffers } from './cards/NewOffers/NewOffers'
 import { PermanentEmployment } from './cards/PermanentEmployment/PermanentEmployment'
 import { RemoteWorkModeSplit, OfficeWorkModeSplit, HybridWorkModeSplit } from './cards/WorkMode'
 import { PopularTechnologies } from './cards/PopularTechnologies/PopularTechnologies'
@@ -43,17 +41,15 @@ export const JobMarket: FC<Record<string, never>> = () => {
               xs: '1fr',
               sm: 'repeat(2, 1fr)',
               lg: 'repeat(4, 1fr)',
-              xl: 'repeat(6, 1fr)',
+              xl: 'repeat(12, 1fr)',
             },
             gridTemplateAreas: {
               xs: `
                 "activeOffers"
-                "newOffers"
-                "medianSalary"
+                "cv"
                 "p90salary"
                 "ads-with-range"
                 "permanentEmployment"
-                "cv"
                 "remote-work-mode-split"
                 "hybrid-work-mode-split"
                 "office-work-mode-split"
@@ -62,40 +58,32 @@ export const JobMarket: FC<Record<string, never>> = () => {
                 "job-ads"
               `,
               sm: `
-                "activeOffers newOffers"
-                "medianSalary p90salary"
-                "ads-with-range permanentEmployment"
-                "cv remote-work-mode-split"
+                "activeOffers cv"
+                "p90salary ads-with-range"
+                "permanentEmployment remote-work-mode-split"
                 "hybrid-work-mode-split office-work-mode-split"
                 "required-skills required-skills"
                 "salary-distribution salary-distribution"
                 "job-ads job-ads"
               `,
               lg: `
-                "activeOffers newOffers medianSalary p90salary"
-                "ads-with-range permanentEmployment cv ."
-                "remote-work-mode-split hybrid-work-mode-split office-work-mode-split ."
+                "activeOffers cv p90salary ads-with-range"
+                "permanentEmployment remote-work-mode-split hybrid-work-mode-split office-work-mode-split"
                 "required-skills required-skills job-ads job-ads"
                 "salary-distribution salary-distribution job-ads job-ads"
               `,
               xl: `
-                "activeOffers newOffers medianSalary p90salary ads-with-range permanentEmployment"
-                "required-skills required-skills job-ads job-ads job-ads remote-work-mode-split"
-                "required-skills required-skills job-ads job-ads job-ads hybrid-work-mode-split"
-                "salary-distribution salary-distribution job-ads job-ads job-ads office-work-mode-split"
-                "salary-distribution salary-distribution job-ads job-ads job-ads cv"
+                "activeOffers activeOffers cv cv p90salary p90salary ads-with-range ads-with-range permanentEmployment permanentEmployment remote-work-mode-split remote-work-mode-split"
+                "required-skills required-skills required-skills job-ads job-ads job-ads job-ads job-ads job-ads job-ads hybrid-work-mode-split hybrid-work-mode-split"
+                "required-skills required-skills required-skills job-ads job-ads job-ads job-ads job-ads job-ads job-ads office-work-mode-split office-work-mode-split"
+                "salary-distribution salary-distribution salary-distribution job-ads job-ads job-ads job-ads job-ads job-ads job-ads . ."
+                "salary-distribution salary-distribution salary-distribution job-ads job-ads job-ads job-ads job-ads job-ads job-ads . ."
               `,
             },
           }}
         >
           <Box sx={{ gridArea: 'activeOffers' }}>
             <ActiveOffers />
-          </Box>
-          <Box sx={{ gridArea: 'newOffers' }}>
-            <NewOffers />
-          </Box>
-          <Box sx={{ gridArea: 'medianSalary' }}>
-            <MedianSalary />
           </Box>
           <Box sx={{ gridArea: 'p90salary' }}>
             <P90Salary />
