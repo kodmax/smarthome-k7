4c33dfdb10320e704b55ecb91437e6e21b5f89cb
marcinm <mm.kodmax@gmail.com>
2026-08-28 08:41:35 +0200

    Improve service.sh for systemd with exec, set -eu, and relative paths.
    Change into the repo before starting Node so preload and entrypoint use ./apps/service/dist paths.

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-/home/pi/.nvm/versions/node/v26.3.0/bin/node \
-  --require /home/pi/smart-home/smarthome-k7/apps/service/dist/preload.js \
-  /home/pi/smart-home/smarthome-k7/apps/service/dist/index.js
+set -eu
+
+cd /home/pi/smart-home/smarthome-k7
+
+exec /home/pi/.nvm/versions/node/v26.3.0/bin/node \
+  --require ./apps/service/dist/preload.js \
+  ./apps/service/dist/index.js
